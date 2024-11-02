1acc9cdb8cbe287cd8d31b1158867ef6ff165f17
YOGI <62894986+yogiifr@users.noreply.github.com>
2024-11-02 23:08:37 +0700

    feat - powershell is powering

---
+++ PowerShell/Microsoft.Powershell_profile.ps1
@@ -0,0 +1,23 @@
+#Aliases
+Set-Alias tt tree
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias vim nvim
+
+#Prompt
+Clear-Host
+oh-my-posh init pwsh --config 'C:\Users\yogif\OneDrive\Documents\PowerShell\YOGI.omp.json' | Invoke-Expression
+
+#Terminal Icons
+Import-Module Terminal-Icons
+
+#PSReadLine
+Import-Module PSReadLine
+Set-PSReadLineKeyHandler -Key Tab -Function Complete
+Set-PSReadLineOption -PredictionViewStyle List
+
+#Functions
+function whereis ($command) {
+    Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
